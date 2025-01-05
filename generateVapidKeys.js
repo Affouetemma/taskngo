af98029b0d0e69c204f4d6dd89ab4bb4d43718b0
Loody <loody@Affouets-iMac.local>
2025-01-04 21:05:30 -0500

    Update gitignore

--- generateVapidKeys.js
+++
@@ -1,7 +0,0 @@
-const webpush = require('web-push');
-
-// Generate VAPID Keys
-const vapidKeys = webpush.generateVAPIDKeys();
-
-console.log('VAPID Public Key:', vapidKeys.publicKey);
-console.log('VAPID Private Key:', vapidKeys.privateKey);
